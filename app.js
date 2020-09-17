ba372d35db97781158a962ed1aadebb78750b67d
admin <admin@DAS01178.local>
2020-09-17 16:12:00 +0100

    Successfull data input into mongoDB

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/information';
+var str = "";
+
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.get("/", function (request, response){
-		response.sendFile(__dirname+"/views/index.html");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/information")
+var nameSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	email: String
+});
+var User = mongoose.model("User", nameSchema)
+
+app.get("/", function (req, res){
+		res.render('index');
 });
 
-app.get('/database', function(req, res){
-   res.render('database');
+app.post('/details', (req, res) => {
+	var Details = new User(req.body);
+	Details.save()
+		.then(item => {
+			res.send("Item successfully saved to the database. Go to /database.")
+		})
 });
 
-app.get('/data', function(req, res){
-	 var firstname = req.query.firstname;
-
-	 if (firstname != "") {
-		 res.send("Your name is " + firstname);
-	 } else {
-		 	res.send("Please provide your first name")
-	 }
+app.route('/database').get(function(req, res) {
+   MongoClient.connect('mongodb://localhost', function(err, client) {
+		 	 var db = client.db('information');
+       var collection = db.collection('information');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Firstname " + item.Employeeid + "</br>";
+           }
+       }, function(err) {
+           res.send(err);
+           db.close();
+          }
+       );
+   });
 });
+
+
+
+
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!');
